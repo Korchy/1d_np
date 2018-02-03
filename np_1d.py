0beab76f4ee6e12e998eeece833e24a852a88bf2
Nikita Akimov <korchiy@yandex.ru>
2018-02-03 22:26:36 +0300

    update

--- np_1d.py
+++ np_1d.py
@@ -24,6 +24,7 @@ class Np1d:
     __selection = []
     __selectionlocation = None
     __mode = []   # list: SELECT, TRANSLATE, NAVIGATE
+    __replicationpoints = []
 
     @staticmethod
     def saveenvironment():
@@ -151,10 +152,92 @@ class Np1d:
         __class__.__mode.append(mode)
 
     @staticmethod
+    def addreplicationpoint(instance = False):
+        __class__.__replicationpoints.append((__class__.anchor().location.copy(), instance))
+
+    @staticmethod
+    def replicateonpoints():
+        __class__.savecursor3dposition()
+        __class__.restoreselection()
+        __class__.anchor().select = False
+        for point in __class__.__replicationpoints:
+            bpy.ops.object.duplicate(linked=point[1])
+            bpy.context.area.spaces.active.cursor_location = point[0] - __class__.__anchoroffset
+            bpy.ops.view3d.snap_selected_to_cursor(use_offset=True)
+        __class__.restorecursor3dposition()
+
+    @staticmethod
     def clear():
         __class__.__mode = []
         __class__.setenvironment()
         __class__.anchor().select = False
+        __class__.__replicationpoints = []
+
+
+class Np1dCCCopy(bpy.types.Operator):
+    bl_idname = 'np1d.cccopy'
+    bl_label = 'NP_1D_CCCopy'
+    bl_description = 'NP_1D_CCCopy'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def modal(self, context, event):
+        if event.type == 'LEFTMOUSE':
+            if Np1d.getmode() == 'SELECT':
+                Np1d.restoreselection()
+                Np1d.anchor().select = True
+                Np1d.saveanchorselectionoffset()
+                Np1d.setmode('TRANSLATE')
+                bpy.ops.transform.translate('INVOKE_DEFAULT')
+            elif Np1d.getmode() == 'TRANSLATE':
+                Np1d.addreplicationpoint(not event.alt)
+                bpy.ops.transform.translate('INVOKE_DEFAULT')
+        elif event.type in ('RET', 'NUMPAD_ENTER'):
+            if Np1d.getmode() == 'SELECT':
+                bpy.ops.transform.translate('INVOKE_DEFAULT')
+            elif Np1d.getmode() == 'TRANSLATE':
+                Np1d.selectiontostartlocation()
+                Np1d.replicateonpoints()
+                Np1d.clear()
+                return {'FINISHED'}
+        elif event.type == 'SPACE':
+            if event.value == 'RELEASE':
+                Np1d.setmode('NAVIGATE')
+                Np1d.selectiontostartlocation()
+                Np1d.setenvironment()
+            elif event.value == 'PRESS':
+                Np1d.setmode(Np1d.getmode(-1))
+                Np1d.setlocalenvironment()
+                Np1d.restoreselection()
+                anchor = Np1d.anchor()
+                anchor.select = True
+                bpy.context.scene.objects.active = anchor
+                Np1d.anchortomousecursor()
+                Np1d.selectiontoanchor()
+                bpy.ops.transform.translate('INVOKE_DEFAULT')
+                return {'INTERFACE'}
+        elif event.type == 'RIGHTMOUSE':
+            if Np1d.getmode() != 'NAVIGATE':
+                bpy.ops.transform.translate('INVOKE_DEFAULT')
+        elif event.type == 'ESC':
+            Np1d.selectiontostartlocation()
+            return {'CANCELLED'}
+        return {'PASS_THROUGH'}
+
+    def execute(self, context):
+        Np1d.saveenvironment()
+        Np1d.saveselection()
+        Np1d.setlocalenvironment()
+        bpy.ops.object.select_all(action='DESELECT')
+        Np1d.anchor().select = True
+        Np1d.anchortomousecursor()
+        Np1d.setmode('SELECT')
+        bpy.ops.transform.translate('INVOKE_DEFAULT')
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def cancel(self, context):
+        Np1d.clear()
+        return None
 
 
 class Np1dZZMove(bpy.types.Operator):
@@ -219,17 +302,6 @@ class Np1dZZMove(bpy.types.Operator):
     def cancel(self, context):
         Np1d.clear()
         return None
-
-
-class Np1dCCCopy(bpy.types.Operator):
-    bl_idname = 'np1d.cccopy'
-    bl_label = 'NP_1D_CCCopy'
-    bl_description = 'NP_1D_CCCopy'
-    bl_options = {'REGISTER', 'UNDO'}
-
-    status = None
-
-    pass
 
 
 def register():
